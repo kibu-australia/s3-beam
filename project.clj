2de0eff8633fea7a7b3ce8cec6d2ff952489c796
Thomas Crowley <crowley@kibu.com.au>
2016-04-05 13:05:02 +1000

    0.5.2-0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.martinklepsch/s3-beam "0.5.2-SNAPSHOT"
+(defproject kibu/s3-beam "0.5.2-0"
   :author "Martin Klepsch <http://www.martinklepsch.org>"
   :description "CORS Upload to S3 via Clojure(script)"
   :url "http://github.com/martinklepsch/s3-beam"
